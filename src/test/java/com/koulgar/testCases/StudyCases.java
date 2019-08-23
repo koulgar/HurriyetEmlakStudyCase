dae7031ef10760a5e4cc3e22187f773691d1f37c
koulgar <dogucandurmuslar@gmail.com>
2019-08-23 19:35:13 +0300

    Environment Setup

---
+++ src/test/java/com/koulgar/testCases/StudyCases.java
@@ -0,0 +1,18 @@
+package com.koulgar;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class StudyCases {
+
+    @BeforeTest
+    
+    @Test
+    public void shouldAnswerWithTrue() {
+
+        assertTrue( true );
+
+    }
+}
